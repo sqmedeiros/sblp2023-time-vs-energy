4ac4973e3b156923b9776693b9a16639f2484ac0
borgesnogueira <borgesnogueira@gmail.com>
2024-07-24 10:27:31 -0300

    substituindo codigo que trava

---
+++ 1621-Distinct_Numbers/java/rand30/entry_9955535.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+ 
+//        int n = scanner.nextInt();
+        int n = Integer.parseInt(reader.readLine().trim());
+        HashSet<String> set = new HashSet<>();
+        int count = 0;
+ 
+        String[] nums = reader.readLine().trim().split(" ");
+        for (String i : nums) {
+//            int c = scanner.nextInt();
+            if(set.contains(i)) {
+                continue;
+            }
+ 
+            set.add(i);
+            count++;
+        }
+ 
+        System.out.println(count);
+    }
+}
