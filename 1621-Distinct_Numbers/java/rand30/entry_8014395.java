4803dab5322b45dad89e6627741398d8239a45a8
Sergio <sqmedeiros@gmail.com>
2024-09-05 14:58:57 +0200

    Adding annotation about exception in Java program

--- 1621-Distinct_Numbers/java/rand30/entry_8014395.java
+++ 1621-Distinct_Numbers/java/rand30/entry_8014395.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class entry_8014395 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
